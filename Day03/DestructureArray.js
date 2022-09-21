8cfee64a99c4065cc805a08e0be85143df68986a
putri13 <58936058+putri13@users.noreply.github.com>
2022-09-21 14:46:45 +0700

    first commit

---
+++ Day03/DestructureArray.js
@@ -0,0 +1,11 @@
+const programming = ['java','javascript','python','sql','oracle','postgress']
+const [python,javascript,java,sql,...database]=programming
+console.log();
+
+const object = {
+    firstName : 'Jhon',
+    LastName : 'Doe',
+    Age : 14
+}
+const {firstName,LastName,Age} = object
+console.log();
